95ff7f74dc011efc2bad3b6d58254e1361ca5af6
Raymond Sanchez <125612994+RPSanchez@users.noreply.github.com>
2023-02-18 17:09:27 +0900

    Update navbar.tsx
    v1.1 (Bugfix)

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,8 @@
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import Image from 'next/image';
 
 function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ');
@@ -14,38 +16,41 @@ export default function Navbar() {
           <div className="absolute inset-y-0 left-0 flex items-center"></div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex items-center">
-              <a href="/">
-                <img
-                  className="block h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt="Your Company"
-                />
-              </a>
-              <div className="ml-4 sm:ml-6 sm:flex sm:space-x-8 items-center">
-                <a
-                  href="/leaderboard"
-                  className="border-indigo-500 text-gray-900 text-sm font-medium hover:border-gray-300 hover:text-gray-700"
-                  style={{ transition: 'color 0.3s ease' }}
-                  onMouseOver={(e) => {
-                    const button = e.target as HTMLElement;
-                    const rect = button.getBoundingClientRect();
-                    const x = e.clientX - rect.left;
-                    const y = e.clientY - rect.top;
-                    const centerX = rect.width / 2;
-                    const centerY = rect.height / 2;
-                    const deltaX = (centerX - x) / centerX;
-                    const deltaY = (centerY - y) / centerY;
-                    const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2);
-                    const color = `rgba(79, 70, 229, ${distance})`;
-                    button.style.color = color;
-                  }}
-                  onMouseOut={(e) => {
-                    const button = e.target as HTMLElement;
-                    button.style.color = 'black';
-                  }}
-                >
-                  Leaderboard
+              <Link href="/">
+                <a>
+                  <Image
+                    className="block h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    alt="Your Company"
+                  />
                 </a>
+              </Link>
+              <div className="ml-4 sm:ml-6 sm:flex sm:space-x-8 items-center">
+                <Link href="/leaderboard">
+                  <a
+                    className="border-indigo-500 text-gray-900 text-sm font-medium hover:border-gray-300 hover:text-gray-700"
+                    style={{ transition: 'color 0.3s ease' }}
+                    onMouseOver={(e) => {
+                      const button = e.target as HTMLElement;
+                      const rect = button.getBoundingClientRect();
+                      const x = e.clientX - rect.left;
+                      const y = e.clientY - rect.top;
+                      const centerX = rect.width / 2;
+                      const centerY = rect.height / 2;
+                      const deltaX = (centerX - x) / centerX;
+                      const deltaY = (centerY - y) / centerY;
+                      const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2);
+                      const color = `rgba(79, 70, 229, ${distance})`;
+                      button.style.color = color;
+                    }}
+                    onMouseOut={(e) => {
+                      const button = e.target as HTMLElement;
+                      button.style.color = 'black';
+                    }}
+                  >
+                    Leaderboard
+                  </a>
+                </Link>
               </div>
             </div>
           </div>

4050bae1a1a71985f2e547289059a1a4ed685ed4
Raymond Sanchez <125612994+RPSanchez@users.noreply.github.com>
2023-02-18 14:58:58 +0900

    Update navbar.tsx
    Pass 1

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
-function classNames(...classes) {
+function classNames(...classes: any[]) {
   return classes.filter(Boolean).join(' ')
 }
 
@@ -14,21 +14,41 @@ export default function Navbar() {
           <div className="absolute inset-y-0 left-0 flex items-center">
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              <img
-                className="block h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt="Your Company"
-              />
-            </div>
-            <div className="ml-4 sm:ml-6 sm:flex sm:space-x-8">
+            <div className="flex items-center">
               {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
-              <a
-                href="/leaderboard"
-                className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-900"
-              >
-                Leaderboard
+              <a href="/">
+                <img
+                  className="block h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt="Your Company"
+                />
               </a>
+              <div className="ml-4 sm:ml-6 sm:flex sm:space-x-8 items-center">
+                <a
+                  href="/leaderboard"
+                  className="inline-flex items-center h-full px-1 pt-1 text-sm font-medium text-gray-900"
+                  style={{ transition: 'color 0.3s ease' }}
+                  onMouseOver={(e) => {
+                    const button = e.target as HTMLElement;
+                    const rect = button.getBoundingClientRect();
+                    const x = e.clientX - rect.left;
+                    const y = e.clientY - rect.top;
+                    const centerX = rect.width / 2;
+                    const centerY = rect.height / 2;
+                    const deltaX = (centerX - x) / centerX;
+                    const deltaY = (centerY - y) / centerY;
+                    const distance = Math.sqrt(deltaX ** 2 + deltaY ** 2);
+                    const color = `rgba(79, 70, 229, ${distance})`;
+                    button.style.color = color;
+                  }}
+                  onMouseOut={(e) => {
+                    const button = e.target as HTMLElement;
+                    button.style.color = 'black';
+                  }}
+                >
+                  <div className="flex items-center transform -translate-y-1">Leaderboard</div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
@@ -36,6 +56,5 @@ export default function Navbar() {
       {/* Spacer */}
       <div className='h-16 w-full'></div>
     </>
-
   )
 }

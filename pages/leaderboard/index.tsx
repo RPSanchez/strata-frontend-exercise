8b8f505ed6e7ed560285bf4dba9ccb0af6d08cd4
Raymond Sanchez <125612994+RPSanchez@users.noreply.github.com>
2023-02-19 17:13:44 +0900

    v2.3
    Post-release fix for loading display. +10~ minutes of dev time.

--- pages/leaderboard/index.tsx
+++ pages/leaderboard/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect, useCallback } from "react";
+import { FC, useState, useEffect, useCallback, lazy } from "react";
 import Link from "next/link";
 import styles from "styles/Leaderboard.module.css";
 import React from "react";
@@ -39,6 +39,7 @@ const LeaderboardItem: FC<{
               className={styles.profileImage}
               width={50}
               height={50}
+              priority
             />
           </a>
         </Link>
@@ -67,6 +68,7 @@ const Leaderboard: FC = () => {
   const [leaderboard, setLeaderboard] = useState<LeaderboardEntry[]>([]);
   const [likedUsers, setLikedUsers] = useState<Record<string, boolean>>({});
   const [mousePosition, setMousePosition] = useState<{ x: number; y: number } | null>(null);
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchLeaderboard = async () => {
@@ -76,6 +78,7 @@ const Leaderboard: FC = () => {
         const sortedLeaderboard = leaderboard.sort((a: { score: number }, b: { score: number }) => b.score - a.score);
         setLeaderboard(sortedLeaderboard);
         localStorage.setItem("leaderboard", JSON.stringify(sortedLeaderboard));
+        setIsLoaded(true);
       } catch (err) {
         const cachedLeaderboard = localStorage.getItem("leaderboard");
         if (cachedLeaderboard) {
@@ -123,60 +126,62 @@ const Leaderboard: FC = () => {
 
   return (
     <div className="isolate bg-white">
-      <div
-        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-      >
-        <svg
-          className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-          viewBox="0 0 1500 678"
-        >
-          <path
-            fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
-            fillOpacity=".3"
-            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-          />
-          <defs>
-            <linearGradient
-              id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
-              x1="926.62"
-              x2="-102.215"
-              y1=".831"
-              y2="85."
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#9089FC" />
-              <stop offset={1} stopColor="#FF80B5" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </div>
-      <div className={styles.container}>
-        <div className={styles.leaderboard}>
-          <ul className={styles.list} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
-            <li className={`${styles.listItem} ${styles.headerItem}`}>
-              <span className={styles.headerNumber}>#</span>
-              <span className={styles.headerName}>name</span>
-              <span className={styles.headerScore}>score</span>
-            </li>
-            {leaderboard.map(({ username, profileImage, score }, index) => (
-              <LeaderboardItem
-                key={`${username}-${index}-${likedUsers[username]}`}
-                entry={{ username, profileImage, liked: likedUsers[username], score }}
-                index={index}
-                toggleLikedUser={toggleLikedUser}
-                style={
-                  mousePosition
-                    ? {
-                        boxShadow: `${mousePosition.x}px ${mousePosition.y}px 10px rgba(0, 0, 0, 0.2)`,
-                      }
-                    : {}
-                }
-              />
-            ))}
-          </ul>
+      {!isLoaded && (
+        <div className={styles.loadingCircle}>
+          <span className={styles.loadingText}>Loading...</span>
         </div>
-        <div className={styles.boxShadow} style={{ left: 0, top: 0 }} />
-      </div>
+      )}
+      {isLoaded && (
+        <>
+          <div
+            className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+          >
+            <svg
+              className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
+              viewBox="0 0 1500 678"
+            >
+              <path
+                fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
+                fillOpacity=".3"
+                d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+              />
+              <defs>
+                <linearGradient
+                  id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
+                  x1="926.62"
+                  x2="-102.215"
+                  y1=".831"
+                  y2="85."
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#9089FC" />
+                  <stop offset={1} stopColor="#FF80B5" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>
+          <div className={styles.container}>
+            <div className={styles.leaderboard}>
+              <ul className={styles.list} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
+                <li className={`${styles.listItem} ${styles.headerItem}`}>
+                  <span className={styles.headerNumber}>#</span>
+                  <span className={styles.headerName}>name</span>
+                  <span className={styles.headerScore}>score</span>
+                </li>
+                {leaderboard.map(({ username, profileImage, score }, index) => (
+                  <LeaderboardItem
+                    key={`${username}-${index}-${likedUsers[username]}`}
+                    entry={{ username, profileImage, liked: likedUsers[username], score }}
+                    index={index}
+                    toggleLikedUser={toggleLikedUser}
+                  />
+                ))}
+              </ul>
+            </div>
+            <div className={styles.boxShadow} style={{ left: 0, top: 0 }} />
+          </div>
+        </>
+      )}
     </div>
   );
 };
